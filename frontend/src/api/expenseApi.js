e7e10b871a3dc343cb2c2efb460d483eaf328f95
jananishankar567 <jananishankar65@gmail.com>
2026-04-08 00:02:01 +0530

    Update expenseApi.js

--- frontend/src/api/expenseApi.js
+++ frontend/src/api/expenseApi.js
@@ -58,7 +58,7 @@
 
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL = "https://personal-expense-tracking-system.onrender.com/api" || "http://localhost:5000/api";
 
 const getToken = () => localStorage.getItem("token");
 
